3dd32f5f5bee1e3682f3d2ede39c9a77501f60ab
[dyad] <lauranunes1306@gmail.com>
2025-10-22 18:33:23 -0300

    [dyad] Fixed critical React loading error and removed invalid image preload link from index.html. - wrote 3 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React, { Suspense } from "react";
+import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Implementação de Code Splitting
@@ -17,13 +17,13 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Suspense fallback={<div className="flex min-h-screen items-center justify-center">Cargando...</div>}>
+        <React.Suspense fallback={<div className="flex min-h-screen items-center justify-center">Cargando...</div>}>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </Suspense>
+        </React.Suspense>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
